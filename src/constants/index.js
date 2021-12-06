8b2ebbe857d00ee435b74a79d049037337b10db9
Shirish Saxena <6762915+ShirishSaxena@users.noreply.github.com>
2021-12-06 12:19:11 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const API_BASE_URL = 'http://213.32.15.53:5050/api';
+export const ACCESS_TOKEN = '9a02115a835ee03d5fb83cd8a468ea33e4090aaaec87f53c9fa54512bbef4db8dc656c82a315fa0c785c08b0134716b81ddcd0153d2a7556f2e154912cf5675f';
+
+export const POLL_LIST_SIZE = 30;
+export const MAX_CHOICES = 6;
+export const POLL_QUESTION_MAX_LENGTH = 140;
+export const POLL_CHOICE_MAX_LENGTH = 40;
+
+export const NAME_MIN_LENGTH = 4;
+export const NAME_MAX_LENGTH = 40;
+
+export const USERNAME_MIN_LENGTH = 3;
+export const USERNAME_MAX_LENGTH = 15;
+
+export const EMAIL_MAX_LENGTH = 40;
+
+export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MAX_LENGTH = 20;
